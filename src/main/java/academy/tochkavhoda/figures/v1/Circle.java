a16b78f74066effa329271aaf7579486eec15b2c
Kerus Maksim <gemuzkm@gmail.com>
2022-08-02 10:47:35 +0300

    Circle done

--- src/main/java/academy/tochkavhoda/figures/v1/Circle.java
+++ src/main/java/academy/tochkavhoda/figures/v1/Circle.java
@@ -1,6 +1,90 @@
 package academy.tochkavhoda.figures.v1;
 
 public class Circle {
+    private int radius;
+    private Point center;
 
+    public Circle(Point center, int radius) {
+        this.center = center;
+        this.radius = radius;
+    }
 
+    public Circle(int xCenter, int yCenter, int radius) {
+        this(new Point(xCenter, yCenter), radius);
+    }
+
+    public Circle(int radius) {
+        this(0, 0, radius);
+    }
+
+    public Circle() {
+        this(0, 0, 1);
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void moveTo(int x, int y) {
+        center.setX(x);
+        center.setY(y);
+    }
+
+    public void moveTo(Point point) {
+        moveTo(point.getX(), point.getY());
+    }
+
+    public void moveRel(int dx, int dy) {
+        center.moveRel(dx, dy);
+    }
+
+    public void resize(double ratio) {
+        radius = (int) (radius * ratio);
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public boolean isInside(int x, int y) {
+        return center.getX() - radius <= x && x <= center.getX() + radius && center.getY() - radius <= y && y <= center.getY() + radius;
+    }
+
+    public boolean isInside(Point point) {
+        return isInside(point.getX(), point.getY());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Circle circle = (Circle) o;
+
+        if (radius != circle.radius) return false;
+        return center.equals(circle.center);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = radius;
+        result = 31 * result + center.hashCode();
+        return result;
+    }
 }
