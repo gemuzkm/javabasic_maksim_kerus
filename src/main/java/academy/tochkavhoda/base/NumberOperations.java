a6b883c7e71ee6c34d14ab1b009f9bdd755fdc3f
Kerus Maksim <gemuzkm@gmail.com>
2022-08-03 10:51:05 +0300

    NumberOperations done

---
+++ src/main/java/academy/tochkavhoda/base/NumberOperations.java
@@ -0,0 +1,42 @@
+package academy.tochkavhoda.base;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class NumberOperations {
+    public static Integer find(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public static Integer find(double[] array, double value, double eps) {
+        for (int i = 0; i < array.length; i++) {
+            if (Math.abs(array[i] - value) < eps) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public static Double calculateDensity(double weight, double volume, double min, double max) {
+        return (weight * volume) / (max - min);
+    }
+
+    public static Integer find(BigInteger[] array, BigInteger value)    {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public static BigDecimal calculateDensity(BigDecimal weight, BigDecimal volume, BigDecimal min, BigDecimal max) {
+        return (weight.multiply(volume)).divide(max.subtract(min));
+    }
+
+}
