5443834f87b82d644ce2568f16937840afc10280
Kerus Maksim <gemuzkm@gmail.com>
2022-08-07 13:09:43 +0300

    TestTypes done

--- src/main/java/academy/tochkavhoda/figures/v2/Figure.java
+++ src/main/java/academy/tochkavhoda/figures/v2/Figure.java
@@ -5,11 +5,11 @@ import academy.tochkavhoda.iface.v2.Movable;
 import academy.tochkavhoda.iface.v2.Resizable;
 
 public abstract class Figure implements HasArea, Resizable, Movable {
-    abstract boolean isInside(int x, int y);
+    public abstract boolean isInside(int x, int y);
 
-    boolean isInside(Point point) {
+    public boolean isInside(Point point) {
         return isInside(point.getX(), point.getY());
     }
 
-    abstract double getPerimeter();
+    public abstract double getPerimeter();
 }
